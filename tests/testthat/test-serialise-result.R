7d8d404527e6d3f8383e4e328f1568a7ada771f8
David Neuzerling <david@neuzerling.com>
2022-04-23 05:50:02 +0000

    version 1.2.0

--- tests/testthat/test-serialise-result.R
+++ tests/testthat/test-serialise-result.R
@@ -31,3 +31,18 @@ test_that("custom serialisers are used", {
     "my heart is a fish"
   )
 })
+
+test_that("already_serialised results are not serialised twice", {
+  already_serialised <- structure(
+    list(result = html_response("abc")),
+    "result_calculated" = TRUE,
+    class = "event"
+  )
+  test_config <- structure(list(serialiser = NULL), class = "lambda_config")
+
+  # serialise_result should return the event$result unaltered
+  expect_equal(
+    already_serialised$result,
+    serialise_result(already_serialised, test_config)
+  )
+})
